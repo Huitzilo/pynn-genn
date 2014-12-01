7b07efa9c5510a7367863868f918a9d5172ce9a5
Michael Schmuker <m.schmuker@biomachinelearning.net>
2014-12-01 14:21:43 +0000

    boilerplate code and removing unneeded stuff

--- genn/cells.py
+++ genn/cells.py
@@ -5,11 +5,60 @@ Created on Fri Nov 28 18:56:43 2014
 @author: micha
 """
 
-class Izhikevich(object):
+# These are defined in $GeNNPATH/lib/utils.h and extra_neurons.h
+
+class GeNNNeuron(object):
+    """
+    Base class for GeNN Neurons.
+    """
     pass
 
 class RulkovMapNeuron(object):
+    """
+    Rulkov map-based neurons.
+    """
     pass
 
-class SpikeSourcePoisson(object):
+class PoissonNeurons(object):
+    """
+    Poisson process spike train generator.
+    """
+    pass
+
+class TraubMiles(object):
+    """
+    Traub and Miles Hodgin-Huxley neurons.
+    """
+    pass
+
+class Izhikevich(object):
+    """
+    Izhikevich neurons.
+    """
+    pass
+
+class IzhikevichVar(object):
+    """
+    Izhikevich neurons with variable parameters.
+    """
+    pass
+
+# from extra_neurons.h
+
+class LeakyIF(object):
+    """
+    Leaky Integrate-And-Fire neurons.
+    """
+    pass
+
+class RegularSpiking(object):
+    """
+    A regularly spiking neuron.
+    """
+    pass
+
+class LeakyIntegrate(object):
+    """
+    Leaky Integrate and Fire number two.
+    """
     pass
--- genn/populations.py
+++ genn/populations.py
@@ -6,65 +6,35 @@ Created on Fri Nov 28 19:01:01 2014
 """
 
 from PyNN import common
+from pyNN.parameters import ParameterSpace
 from . import simulator
-from pyNN.standardmodels import StandardCellType
-from pyNN.parameters import ParameterSpace, simplify
-from .recording import Recorder
 
-class Assembly(common.Assembly):
-    _simulator = simulator
+class IDMixin(common.IDMixin):
+    pass
 
-
-class PopulationView(common.PopulationView):
-    _assembly_class = Assembly
-    _simulator = simulator
-
-    def _get_parameters(self, *names):
-        """
-        return a ParameterSpace containing native parameters
-        """
-        pass
-    
-    def _set_parameters(self, parameter_space):
-        """
-        parameter_space should contain native parameters
-        """
-        pass
-    
-    def _set_initial_value_array(self, variable, initial_values):
-        raise NotImplementedError
-        
-    def _get_view(self, selector, label=None):
-        return PopulationView(self, selector, label)
-        
+class BasePopulation(common.BasePopulation):
+    pass
 
 class Population(common.Population):
-    __doc__ = common.Population.__doc__
-    _simulator = simulator
-    _recorder_class = Recorder
-    _assembly_class = Assembly
-
     def _create_cells(self):
+        #TODO
         pass
     
-    def _set_initial_value_array(self, variable, value):
+    def _get_parameters(self):
+        #TODO
+        pass
+    
+    def _set_parameters(self):
+        #TODO
+        pass
+    
+    def _get_view(self):
+        #TODO
         pass
 
-    def _get_view(self, selector, label=None):
-        return PopulationView(self, selector, label)            
-    
-    def _get_parameters(self, *names):
-        """
-        return a ParameterSpace containing native parameters
-        """
-        parameter_dict = {}
-        for name in names:
-            value = simplify(getattr(self.brian_group, name))
-            parameter_dict[name] = value
-        return ParameterSpace(parameter_dict, shape=(self.size,))
-        
-    def _set_parameters(self, parameter_space):
-        """parameter_space should contain native parameters"""
-        parameter_space.evaluate(simplify=False)
-        for name, value in parameter_space.items():
-            setattr(self.brian_group, name, value)
+class PopulationView(common.PopulationView):
+    pass
+
+class Assembly(common.Assembly):
+    pass
+
