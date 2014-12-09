18baf248fd3bb4d1f7ba947ee49f4bb2908d768e
Michael Schmuker <m.schmuker@biomachinelearning.net>
2014-12-09 18:15:46 +0000

    added default and initial parameters for Poisson neurons and TM.

--- genn/cells.py
+++ genn/cells.py
@@ -21,16 +21,32 @@ class PoissonNeurons(BaseModelType):
     """
     Poisson process spike train generator.
     """
-    default_parameters = {}
-    default_initial_values = {}
+    default_parameters = {'rate': 0.1,      # 0 - firing rate
+                          't_refrac': 2.5,  # 1 - seed
+                          'V_spike': 20.,   # 2 - SpikeTime
+                          'V_rest': -60.}
+                          
+    default_initial_values = {'V': 0.,              # 0 - V
+                              'seed': 1234567,      # 1 - seed
+                              'SpikeTime': -10.}    # 2 - SpikeTime
+
     parameter_checks = {}
 
 class TraubMiles(BaseModelType):
     """
     Traub and Miles Hodgin-Huxley neurons.
     """
-    default_parameters = {}
-    default_initial_values = {}
+    default_parameters = {'gNa': 7.15,  # 0 - gNa: Na conductance in 1/(mOhms * cm^2)
+                          'ENa': 50.,   # 1 - ENa: Na equi potential in mV
+                          'gK': 1.43,   # 2 - gK: K conductance in 1/(mOhms * cm^2)
+                          'EK': -95.,   # 3 - EK: K equi potential in mV
+                          'gl': 0.02672,# 4 - gl: leak conductance in 1/(mOhms * cm^2)
+                          'El': -63.563,# 5 - El: leak equi potential in mV
+                          'Cmem':0.143} # 6 - Cmem: membr. capacity density in muF/cm^2
+    default_initial_values = {'E':0.,               # 0 - membrane potential E
+                              'p_Na_m': 0.529324,   # 1 - prob. for Na channel activation m
+                              'p_Na_h': 0.3176767,  # 2 - prob. for not Na channel blocking h
+                              'p_K_n': 0.5961207}   # 3 - prob. for K channel activation n
     parameter_checks = {}
 
 class Izhikevich(BaseModelType):
