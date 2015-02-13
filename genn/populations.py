29ba9618dceb44358d513d5676cf88292e146495
Michael Schmuker <m.schmuker@biomachinelearning.net>
2015-02-13 16:18:35 +0000

    filled up populations, projections and recording from mock

--- genn/populations.py
+++ genn/populations.py
@@ -5,57 +5,98 @@ Created on Fri Nov 28 19:01:01 2014
 @author: micha
 """
 
-from PyNN import common
-from pyNN.parameters import ParameterSpace
+import numpy
+from pyNN import common
+from pyNN.parameters import ParameterSpace, simplify
+from pyNN.standardmodels import StandardCellType
 from . import simulator
-
-class IDMixin(common.IDMixin):
-    pass
-
-class BasePopulation(common.BasePopulation):
-    def get(self, parameter_names, gather=False):
-        pass
-    
-    def set(self, **parameters):
-        pass
-    
-    def initialize(self, **initial_values):
-        pass
-    
-
-class Population(common.Population):
-    
-    def _recorder_class(self):
-        # needs to return a recorder
-        pass
-    
-    def _create_cells(self):
-        # number of neurons is in self.size
-        # parameterised cell class is in self.celltype
-        # name is in self.label
-        # need to provide something for self._recorder_class
-        # need to create array self.all_cells
-        pass
-
-    def initialize(self, **initial_values):
-        #initial_values is a dict
-        pass
-    
-    def _get_parameters(self):
-        #TODO
-        pass
-    
-    def _set_parameters(self):
-        #TODO
-        pass
-    
-    def _get_view(self):
-        #TODO
-        pass
-
-class PopulationView(common.PopulationView):
-    pass
+from .recording import Recorder
 
 class Assembly(common.Assembly):
-    pass
+    _simulator = simulator
 
+
+class PopulationView(common.PopulationView):
+    _assembly_class = Assembly
+    _simulator = simulator
+
+    def _get_parameters(self, *names):
+        """
+        return a ParameterSpace containing native parameters
+        """
+        parameter_dict = {}
+        for name in names:
+            value = self.parent._parameters[name]
+            if isinstance(value, numpy.ndarray):
+                value = value[self.mask]
+            parameter_dict[name] = simplify(value)
+        return ParameterSpace(parameter_dict, shape=(self.size,)) # or local size?
+
+    def _set_parameters(self, parameter_space):
+        """parameter_space should contain native parameters"""
+        #ps = self.parent._get_parameters(*self.celltype.get_native_names())
+        for name, value in parameter_space.items():
+            self.parent._parameters[name][self.mask] = value.evaluate(simplify=True)
+            #ps[name][self.mask] = value.evaluate(simplify=True)
+        #ps.evaluate(simplify=True)
+        #self.parent._parameters = ps.as_dict()
+
+    def _set_initial_value_array(self, variable, initial_values):
+        pass
+
+    def _get_view(self, selector, label=None):
+        return PopulationView(self, selector, label)
+
+
+
+class Population(common.Population):
+    __doc__ = common.Population.__doc__
+    _simulator = simulator
+    _recorder_class = Recorder
+    _assembly_class = Assembly
+
+    def _create_cells(self):
+        id_range = numpy.arange(simulator.state.id_counter,
+                                simulator.state.id_counter + self.size)
+        self.all_cells = numpy.array([simulator.ID(id) for id in id_range],
+                                     dtype=simulator.ID)
+        def is_local(id):
+            return (id % simulator.state.num_processes) == simulator.state.mpi_rank
+        self._mask_local = is_local(self.all_cells)
+        
+        if isinstance(self.celltype, StandardCellType):
+            parameter_space = self.celltype.native_parameters
+        else:
+            parameter_space = self.celltype.parameter_space
+        parameter_space.shape = (self.size,)
+        parameter_space.evaluate(mask=self._mask_local, simplify=False)
+        self._parameters = parameter_space.as_dict()
+        
+        for id in self.all_cells:
+            id.parent = self
+        simulator.state.id_counter += self.size
+
+    def _set_initial_value_array(self, variable, initial_values):
+        pass
+
+    def _get_view(self, selector, label=None):
+        return PopulationView(self, selector, label)
+
+    def _get_parameters(self, *names):
+        """
+        return a ParameterSpace containing native parameters
+        """
+        parameter_dict = {}
+        for name in names:
+            parameter_dict[name] = simplify(self._parameters[name])
+        return ParameterSpace(parameter_dict, shape=(self.local_size,))
+
+    def _set_parameters(self, parameter_space):
+        """parameter_space should contain native parameters"""
+        #ps = self._get_parameters(*self.celltype.get_native_names())
+        #ps.update(**parameter_space)
+        #ps.evaluate(simplify=True)
+        #self._parameters = ps.as_dict()
+        parameter_space.evaluate(simplify=False, mask=self._mask_local)
+        for name, value in parameter_space.items():
+            self._parameters[name] = value
--- genn/recording.py
+++ genn/recording.py
@@ -4,22 +4,36 @@ Created on Thu Dec  4 22:51:42 2014
 
 @author: micha
 """
-import pyNN.recording 
+import numpy
+from pyNN import recording
+from . import simulator
 
-class Recorder(pyNN.recording.Recorder):
-    
-    def _record(self, variable, new_ids, sampling_interval):
+
+class Recorder(recording.Recorder):
+    _simulator = simulator
+
+    def _record(self, variable, new_ids, sampling_interval=None):
         pass
-    
+
+    def _get_spiketimes(self, id):
+        return numpy.array([id, id+5], dtype=float) % self._simulator.state.t
+
+    def _get_all_signals(self, variable, ids, clear=False):
+        # assuming not using cvode, otherwise need to get times as well and use IrregularlySampledAnalogSignal
+        n_samples = int(round(self._simulator.state.t/self._simulator.state.dt)) + 1
+        return numpy.vstack((numpy.random.uniform(size=n_samples) for id in ids)).T
+
+    def _local_count(self, variable, filter_ids=None):
+        N = {}
+        if variable == 'spikes':
+            for id in self.filter_recorded(variable, filter_ids):
+                N[int(id)] = 2
+        else:
+            raise Exception("Only implemented for spikes")
+        return N
+
+    def _clear_simulator(self):
+        pass
+
     def _reset(self):
-        pass
-    
-    def get(self, variables, gather=False, filter_ids=None, clear=False,
-            annotations=None):
-        pass
-    
-    def clear(self):
-        pass
-    
-    def count(self, variable, gather=True, filter_ids=None):
         pass
